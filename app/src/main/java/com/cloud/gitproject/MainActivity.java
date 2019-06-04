34c2c2d7f73d0ee07717fa7343453017688d4bd8
zhangxiaoyun <435678288@qq.com>
2019-06-04 20:45:31 +0800

    create a new gitProject  modify

--- app/src/main/java/com/cloud/gitproject/MainActivity.java
+++ app/src/main/java/com/cloud/gitproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.cloud.gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.tv_content).setOnClickListener(view->{
+            Toast.makeText(this,"hello world",Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
