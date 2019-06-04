c44bd6e0fb013040808300480f5ddc82d8a09443
zhangxiaoyun <435678288@qq.com>
2019-06-04 21:11:22 +0800

    create a new gitProject  modify4

--- app/src/main/java/com/cloud/gitproject/MainActivity.java
+++ app/src/main/java/com/cloud/gitproject/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.tv_content).setOnClickListener(view->{
-            //Toast.makeText(this,"hello world",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"hello world",Toast.LENGTH_SHORT).show();
         });
 
     }
